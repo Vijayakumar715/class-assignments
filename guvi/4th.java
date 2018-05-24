045caa30099ded14b3c88cdf9af3204398ef21a4
Vijayakumar715 <34677114+Vijayakumar715@users.noreply.github.com>
2018-05-24 18:55:39 +0530

    Update 4th.java

--- guvi/4th.java
+++ guvi/4th.java
@@ -4,18 +4,18 @@ import java.io.*;
 public class NNumber{
 
      public static void main(String []args){
-         int N,j,temp,i;
+         int d,j,temp,i;
          
          Scanner in=new Scanner(System.in);
-         N=in.nextInt();
-         int a[]=new int[N];
-         for(i=0;i<N;i++)
+         d=in.nextInt();
+         int a[]=new int[d];
+         for(i=0;i<d;i++)
          a[i]=in.nextInt();
         
-         for(i=0;i<N;i++)
+         for(i=0;i<d;i++)
          {
              int count=0;
-             for(j=0;j<N;j++)
+             for(j=0;j<d;j++)
              {
                  if(a[i]==a[j])
                 {
