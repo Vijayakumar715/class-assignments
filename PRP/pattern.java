927c8f18c302a97f81954f95dafe04670986e1b3
Vijayakumar715 <34677114+Vijayakumar715@users.noreply.github.com>
2018-07-04 15:09:49 +0530

    Create pattern.java

---
+++ PRP/pattern.java
@@ -0,0 +1,12 @@
+public class MyClass {
+    public static void main(String args[]) {
+        int i;
+String str="HAI";
+for(i=0;i<str.length();i++)
+{
+            System.out.println(str.substring(0,i+1));
+
+}
+
+    }
+}
